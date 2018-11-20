a6d1b5a7111c4cea18e314746ff8397c64987048
Mario Baxter <mariobax@stanford.edu>
2018-11-20 15:29:03 -0800

    Beta version of Possetions Area

--- app/src/main/java/edu/stanford/cs108/bunnyworld/Classes/MainClasses/PossessionsArea.java
+++ app/src/main/java/edu/stanford/cs108/bunnyworld/Classes/MainClasses/PossessionsArea.java
@@ -1,4 +1,29 @@
 package edu.stanford.cs108.bunnyworld.Classes.MainClasses;
 
+import java.util.HashSet;
+
 public class PossessionsArea {
+
+    private HashSet<Shape> shapes;
+    private int numShapes;
+
+    public PossessionsArea() {
+        shapes = new HashSet<>();
+    }
+
+    public void addShape(Shape shape) {
+        shapes.add(shape);
+    }
+
+    public void removeShape(Shape shape) {
+        shapes.remove(shape);
+    }
+
+    public int getNumShapes() {
+        return numShapes;
+    }
+
+    public String toString() {
+        return shapes.toString();
+    }
 }
